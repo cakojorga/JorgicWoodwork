f606360c72eeda9218b11cbd41006d18b48ee508
“cakojorga” <bekekosarkas@gmail.com>
2026-07-23 18:25:52 +0200

    v 1.4 image downsizer 4

--- src/utility/gallerySliderData.js
+++ src/utility/gallerySliderData.js
@@ -1,50 +1,59 @@
+import kuhinje from "../assets/kuhinje.jpg"
+import ormari from "../assets/ormari.jpg"
+import vrata from "../assets/vrata.jpg"
+import stepenice from "../assets/stepenice.jpg"
+import dnevne from "../assets/dnevne.jpg"
+import djecije from "../assets/djecije.jpg"
+import kreveti from "../assets/kreveti.jpg";
+import ostalo from "../assets/ostalo.jpg"
+
 export const gallerySliderData = [
   {
     id: 1,
     alt: "Kuhinje",
-    img: "/kuhinje.jpg",
+    img: kuhinje,
     title: "Kuhinje",
   },
   {
     id: 2,
     alt: "Ormari",
-    img: "/ormari.jpg",
+    img: ormari,
     title: "Ormari",
   },
   {
     id: 3,
     alt: "Vrata",
-    img: "/vrata.jpg",
+    img: vrata,
     title: "Vrata",
   },
   {
     id: 4,
     alt: "Stepenice",
-    img: "/stepenice.jpg",
+    img: stepenice,
     title: "Stepenice",
   },
   {
     id: 5,
     alt: "Dnevne sobe",
-    img: "/dnevne.jpg",
+    img: dnevne,
     title: "Dnevne sobe",
   },
   {
     id: 6,
     alt: "Dječije sobe",
-    img: "/djecije.jpg",
+    img: djecije,
     title: "Dječije sobe",
   },
   {
     id: 7,
     alt: "Kreveti",
-    img: "/kreveti.jpg",
+    img: kreveti,
     title: "Kreveti",
   },
   {
     id: 8,
     alt: "Ostali stolarski radovi",
-    img: "/ostalo.jpg",
+    img: ostalo,
     title: "Ostali stolarski radovi",
   },
 ];
